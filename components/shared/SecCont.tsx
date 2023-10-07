204d2f00d47e1de5aff91b22d7cd8699b651cfa3
ishfaqahmed565 <ahmedishfaq737@gmail.com>
2023-10-08 03:34:24 +0600

    props type added

--- components/shared/SecCont.tsx
+++ components/shared/SecCont.tsx
@@ -1,4 +1,5 @@
-export default function SecCont(props) {
+import { PropsWithChildren } from "react";
+export default function SecCont(props: PropsWithChildren) {
 	return (
 		<section className="bg-white dark:bg-gray-900">
 			<div className="py-8 lg:py-16 mx-auto max-w-screen-xl flex flex-col gap-20 ">
--- components/shared/SecTitle.tsx
+++ components/shared/SecTitle.tsx
@@ -1,4 +1,6 @@
-export default function SecTitle(props) {
+import { PropsWithChildren } from "react";
+
+export default function SecTitle(props: PropsWithChildren) {
 	return (
 		<h2 className=" text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-6xl">
 			{props.children}
